74eba49298f9f63c6bab2cd02ae0b2bb7fdab119
MangaCat399 <148962190+MangaCat399@users.noreply.github.com>
2023-12-06 14:59:58 +0100

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+alert("Este programa ha sido hecho por Darío Hita Gómez")
+function Muerte() {
+    document.write("<center><font color='red' face='comic sans ms' size='500px'>¡Has perdido!</font><center>")};
+function ganar() {
+    document.write("<center><font color='green' face='comic sans ms' size='50px'>¡Has ganado!</font><br><p3>Este juego ha sido hecho por Darío Hita Gómez</p3></center>")};
+function Empezar() {alert("¡A comenzar!")};
